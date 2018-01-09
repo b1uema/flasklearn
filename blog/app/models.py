be941e2979f3e1178115760e143fe724a86f7378
chinazhenzhen <578233897@qq.com>
2018-01-09 19:23:22 +0800

    加入邮箱验证机制

--- blog/app/models.py
+++ blog/app/models.py
@@ -52,7 +52,7 @@ class User(UserMixin,db.Model):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'confirm': self.id}).decode('utf-8')
 
-    def confirm(self,token):
+    def confirm(self,token):#确认用户账户
         s = Serializer(current_app.config['SECRET_KEY'])
 
         try:
